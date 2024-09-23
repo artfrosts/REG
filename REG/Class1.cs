a4b0f6c31e8bf201b2fbef23e0aa5bcb19ed27c9
Артем <@DESKTOP-ULJ7M14>
2024-09-23 15:55:32 +0300

    Добавьте файлы проекта.

---
+++ REG/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REG
+{
+    public static class DbModelContext
+    {
+        public static REG.REGEntities DB {  get; set; }
+    }
+}
---
+++ REG/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace REG
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DbModelContext.DB = new REGEntities();
+        }
+
+        private void TbEmail_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+            try
+            {
+                DbModelContext.DB.Users.Add(new UserControl()
+                {
+                    UserLogin = TbLogin.Text,
+                    UserPassword = PdPassword.Password,
+                    UserPhone = TbPhone.Text,
+                    UserEmail = TbEmail.Text
+                });
+                DbModelContext.DB.SaveChanges();
+                MessageBox.Show("Данные успешно сохранены",
+                    "Системное сообщение",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(),
+                              "Системное мообщение",
+                               MessageBoxButton.OK, MessageBoxImage.Information);
+
+
+
+
+
+
+
+
+
+            }
+        }
+
+        }
+   
+    }
+}
